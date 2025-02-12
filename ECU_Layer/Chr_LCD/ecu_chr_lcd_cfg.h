ac085e3b1e1759d722506b787d4b1d06a323c407
AhmedShawada <ashawada7@gmail.com>
2025-02-12 20:36:44 +0200

    Implement full LCD control in ECU layer

---
+++ ECU_Layer/Chr_LCD/ecu_chr_lcd_cfg.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   ecu_chr_lcd_cfg.h
+ * Author: ashaw
+ *
+ * Created on August 29, 2024, 12:34 AM
+ */
+
+#ifndef ECU_CHR_LCD_CFG_H
+#define	ECU_CHR_LCD_CFG_H
+
+/*Section       :         Includes                */
+
+
+/*Section       :         Data type Decleration                 */
+
+
+/*Section       :         Macro Decleration                */
+
+/*Section       :         Macro Function Decleration                */
+
+
+
+/*Section       :         Function Decleration                */
+
+#endif	/* ECU_CHR_LCD_CFG_H */
+
