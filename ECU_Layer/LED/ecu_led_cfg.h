3252292e989e073ad1ae73304eef483089035018
AhmedShawada <ashawada7@gmail.com>
2025-02-12 20:38:53 +0200

    Implement LED control in ECU layer

---
+++ ECU_Layer/LED/ecu_led_cfg.h
@@ -0,0 +1,29 @@
+/* 
+ * File:   ecu_led_cfg.h
+ * Author: ashaw
+ *
+ * Created on July 21, 2024, 8:40 PM
+ */
+
+#ifndef ECU_LED_CFG_H
+#define	ECU_LED_CFG_H
+ 
+
+
+
+/*Section       :         Includes                */
+
+/*Section       :         Data type Decleration                 */
+/*Section       :         Macro Decleration                */
+/*Section       :         Macro Function Decleration                */
+/*Section       :         Function Decleration                */
+
+
+
+
+
+
+
+
+#endif	/* ECU_LED_CFG_H */
+
