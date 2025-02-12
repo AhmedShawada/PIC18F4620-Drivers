c879f291ccdafe7370e36a8bc05a052ad7db4403
AhmedShawada <ashawada7@gmail.com>
2025-02-12 20:25:04 +0200

    ADC Driver for Pic18f4620

---
+++ MCAL_Layer/ADC/hal_adc_cfg.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   hal_adc_cfg.h
+ * Author: ahmed-abdallah
+ *
+ * Created on September 24, 2024, 6:18 PM
+ */
+
+#ifndef HAL_ADC_CFG_H
+#define	HAL_ADC_CFG_H
+
+/*Section       :         Includes                */
+
+
+
+/*Section       :         Data type Decleration                 */
+
+
+
+/*Section       :         Macro Decleration                */
+
+
+/*Section       :         Macro Function Decleration                */
+
+/*Section       :         Function Decleration                */
+
+#endif	/* HAL_ADC_CFG_H */
+
